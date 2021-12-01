74250d45d3921136a6fa2f8f3217f377911bccbb
HerrLantz <patriclantz@gmail.com>
2021-12-01 21:40:16 +0100

    Simplified functions

--- src/advent_of_code/day01.clj
+++ src/advent_of_code/day01.clj
@@ -11,17 +11,16 @@
 
 (defn count-increments
   [data-points]
-  (->> (filter (fn [p] (> (second p) (first p))) data-points)
+  (->> (partition 2 1 data-points)
+       (filter (fn [p] (> (second p) (first p))))
        count))
 
 (def solve-part-one
   (->> (parse-input "day01.txt")
-       (partition 2 1)
        count-increments))
 
 (def solve-part-two
   (->> (parse-input "day01.txt")
        (partition 3 1)
        (map (fn [p] (apply + p)))
-       (partition 2 1)
        count-increments))
